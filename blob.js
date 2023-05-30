539f4d196fdf5b7bb65b29b14f73d3d261d6073a
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-05-30 12:59:32 -0400

    Interpolate the velocity of Blob to create smoother movement

--- blob.js
+++ blob.js
@@ -1,14 +1,16 @@
 // Constructor for creating instances of the Blob class
 function Blob(x, y, r) {
   // A vector object stores an x and y component for a position on the screen, etc.
-  this.pos = createVector(x, y); // Position
+  this.pos = createVector(x, y); // Current position of Blob as a vector
   this.r = r; // Radius
+  this.vel = createVector(0, 0); // Current velocity of Blob as a vector
 
   // A method to update the position of the blob based on the mouse position
   this.update = function() {
-    let vel = createVector(mouseX - width / 2, mouseY - height / 2); // Creates a velocity vector using the mouse positions
-    vel.setMag(3); // Sets the magnitude (length) of the vel vector to 3
-    this.pos.add(vel); // Adds vel vector to the this.pos vector
+    let newVel = createVector(mouseX - width / 2, mouseY - height / 2); // Represents the velocity of the Blob based on current mouse position
+    newVel.setMag(3); // Sets the magnitude (length) of the newVel vector to 3
+    this.vel.lerp(newVel, 0.2); // Gradually adjusts (interpolates) the velocity of Blob towards the target velocity represented by newVel - resulting in smoother movement
+    this.pos.add(this.vel); // Updates the position of Blob by adding velocity vector to the position vector
   };
 
   // A method handles the size of the Blob when it overlaps (i.e., 'eats') a small blob
