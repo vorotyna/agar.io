999c551c16ec005681ab28299978240378eb87b7
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-05-30 11:27:10 -0400

    Consider Blob collision with one of blobs

--- sketch.js
+++ sketch.js
@@ -15,9 +15,18 @@ function setup() {
 function draw() {
   background(0);
   translate(width / 2 - blob.pos.x, width / 2 - blob.pos.y); // Translates the origin of the screen so that it appears that the screen (i.e., 'map') moves with the blob
-  blob.show(); // Show the main player blob
-  blob.update(); // Update blob position when mouse moves
-  for (let i = 0; i < blobs.length; i++) { // Show the blobs (i.e., 'food' blobs)
+
+  for (let i = blobs.length - 1; i >= 0; i--) { // Show the blobs (i.e., 'food' blobs)
+    blobs[i].show();
+
+    // If Blob eats one of the blobs, then remove one blob from blobs and Blob grows
+    if (blob.eats(blobs[i])) {
+      blobs[i].splice(0, 1);
+      blob.grows();
+    }
     blobs[i].show();
   }
+
+  blob.show(); // Show the main player blob
+  blob.update(); // Update blob position when mouse moves
 }
