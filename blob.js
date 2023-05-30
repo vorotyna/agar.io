994cd0b98e72f74314e76a6efca526f27e1dd444
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-05-30 10:51:49 -0400

    Create new method to update the position of blob via mouse position

--- blob.js
+++ blob.js
@@ -1,10 +1,18 @@
 // Constructor for creating instances of the Blob class
-function Blob() {
+function Blob(x, y, r) {
   // A vector object stores an x and y component for a position on the screen, etc.
-  this.pos = createVector(width / 2, height / 2); // Position
-  this.r = 64; // Radius
+  this.pos = createVector(x, y); // Position
+  this.r = r; // Radius
 
-  // A method of the Blob class responsible for siplaying the blob on the canvas
+  // A method to update the position of the blob based on the mouse position
+  this.update = function() {
+    let vel = createVector(mouseX, mouseY); // Creates a velocity vector using the mouse positions
+    vel.sub(this.pos); // Subtracts the this.pos vector from the vel vector
+    vel.setMag(3); // Sets the magnitude (length) of the vel vector to 3
+    this.pos.add(vel); // Adds vel vector to the this.pos vector
+  };
+
+  // A method of the Blob class responsible for diplaying the blob on the canvas
   this.show = function() {
     fill(255); // Sets the fill color
     ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2); // Size of the ellipse
--- sketch.js
+++ sketch.js
@@ -1,11 +1,20 @@
 let blob;
 
+let blobs = []; // Holds an array of different blobs
+
 function setup() {
-  createCanvas(600, 600);
-  blob = new Blob();
+  createCanvas(600, 600); // Canavas size
+  blob = new Blob(width / 2, height / 2, 64);
+  for (let i = 0; i < 10; i++) {
+    blobs[i] = new Blob(random(width), random(height), 16); // Creates new blobs in array with random height, random width, and radius of 16
+  }
 }
 
 function draw() {
   background(0);
   blob.show();
+  blob.update();
+  for (let i = 0; i < blobs.length; i++) {
+    blobs[i].show();
+  }
 }
