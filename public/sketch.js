a785f6e72780f3e681662b136cb3b2d9fc595085
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-06-20 15:29:53 -0400

    Add grid lines

--- public/sketch.js
+++ public/sketch.js
@@ -66,23 +66,26 @@ function setup() {
 function draw() {
   background(255); // Sets background colour to white
 
-  // // Draw grid lines in the background
-  // let gridSize = 50; // Size of each grid cell
-  stroke(225); // Color of grid lines
-  // for (let x = 0; x < width; x += gridSize) {
-  //   line(x, 0, x, height); // Vertical lines
-  // }
-  // for (let y = 0; y < height; y += gridSize) {
-  //   line(0, y, width, y); // Horizontal lines
-  // }
-
   translate(width / 2, height / 2); // Translates origin of the coordinate system to the center of the canvas
   let newzoom = 64 / blob.r; // Calculates the new zoom level based on the radius of the blob - the larger the radius, the smaller the zoom level
   zoom = lerp(zoom, newzoom, 0.1); // Interpolates the current zoom level towards the new zoom level gradually
   scale(zoom); // Scale coordinate system to increase the world view
   translate(-blob.pos.x, -blob.pos.y); // Translates the coordinate system to follow the position of the Blob - where world moves in the opposite direction of the Blob position (giving illusion that the world is moving relative to Blob)
 
-  for (let i = blobs.length - 1; i >= 0; i--) { // Loop through all the players 
+  // Draw grid lines in the background
+  let gridSize = 25; // Size of each grid cell
+  stroke(225); // Color of grid lines
+  strokeWeight(0.5);
+  for (let x = -width - 1000; x < width + 1000; x += gridSize) {
+    line(x, -height - 1000, x, height + 1000); // Vertical lines
+  }
+  for (let y = -height - 1000; y < height + 1000; y += gridSize) {
+    line(-width - 1000, y, width + 1000, y); // Horizontal lines
+  }
+
+
+  // Loop through all the players 
+  for (let i = blobs.length - 1; i >= 0; i--) {
     let id = blobs[i].id;
 
     // If it is not the Client's own blob, then draw blob
@@ -99,17 +102,20 @@ function draw() {
     }
   }
 
-  for (let i = 0; i < food.length; i++) { // Show the food blobs
 
+  // Loop through all the food blobs
+  for (let i = 0; i < food.length; i++) {
+    // Show the food blobs on the map
     food[i].show();
 
-    // If Blob eats one of the blobs, then remove one blob from food and Blob grows
+    // If Blob eats one of the food blobs, then remove one food blob from food array and Blob grows
     if (blob.eats(food[i])) {
       food.splice(i, 1); // Remove one element starting at index i 
+
       // Replace with a new food blob
       let x = random(-width, width); // Generate random x value that can be positioned within the canvas area or outside of it
       let y = random(-height, height); // Generate random y value that can be positioned within the canvas area or outside of it
-      food.push(new Blob(x, y, 8, random(200, 255), random(100, 255), random(200, 255))); // Creates new food blobs in food array with random height, random width, radius of 16, and RGB of 55,0,0
+      food.push(new Blob(x, y, 8, random(200, 255), random(100, 255), random(200, 255))); // Creates new food blobs in food array with random height, random width, radius of 16, and random RGB
     }
   }
 
