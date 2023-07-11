d654870783fb78bc76e8a49eb8b1e8e2e1178068
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-07-11 15:20:41 -0400

    Handle magnitude of Blob when it eats something, set max radius for Blob

--- public/blob.js
+++ public/blob.js
@@ -9,12 +9,21 @@ function Blob(x, y, r, colourR, colourG, colourB) {
   this.colourG = colourG; // Fill colour for Blob
   this.colourB = colourB; // Fill colour for Blob
   this.isVisible = true; // Blob is visible (true)
+  this.hasEaten = false; // Blob has eaten something (false)
 
+  let magnitude = 2; // Initial magnitude (i.e., speed) of a new Blob
 
   // A method to update the position of the blob based on the mouse position
   this.update = function() {
     let newVel = createVector(mouseX - width / 2, mouseY - height / 2); // Represents the velocity of the Blob based on current mouse position
-    newVel.setMag(3); // Sets the magnitude (length) of the newVel vector to 3
+
+    // If Blob has eaten something, decrease the magnitude (i.e., slower) relative to the new radius
+    if (this.hasEaten) {
+      magnitude -= this.r / (this.r * 100); // Magnitude of 0.01
+      this.hasEaten = false; // After changing magnitude, set this.hasEaten to false
+    }
+
+    newVel.setMag(magnitude); // Sets the magnitude (length) of the newVel vector
     this.vel.lerp(newVel, 0.2); // Gradually adjusts (interpolates) the velocity of Blob towards the target velocity represented by newVel - resulting in smoother movement
     this.pos.add(this.vel); // Updates the position of Blob by adding velocity vector to the position vector
   };
@@ -24,13 +33,17 @@ function Blob(x, y, r, colourR, colourG, colourB) {
   this.eats = function(other) {
     let d = p5.Vector.dist(this.pos, other.pos); // Calculate distance between Blob and other blob    
 
-    if (d < this.r + other.r) { // If the distance is less than the radius of both Blobs added, means they are overlapping (i.e., being eaten by Blob)
-      // Check if this blob is larger (on top) and eats the other blob
+    if (d < this.r + other.r / (this.r + other.r / this.r)) { // If the distance is less than the radius of both Blobs added, means they are overlapping (i.e., being eaten by Blob)
+      // Check if this blob is larger and can eat the other blob
       let sizeDifference = this.r - other.r;
 
-      if (sizeDifference > 5) {
-        let sum = PI * this.r * this.r + PI * other.r * other.r; // Sum of areas of both blobs (i.e., A = PI * r^2)
-        this.r = sqrt(sum / PI); // Solve for new radius of Blob based on the area previously calculated
+      if (sizeDifference > 3) {
+        // If the radius is smaller or equal to 54
+        if (this.r <= 54) {
+          let sum = PI * this.r * this.r + PI * other.r * other.r; // Sum of areas of both blobs (i.e., A = PI * r^2)
+          this.r = sqrt(sum / PI); // Solve for new radius of Blob based on the area previously calculated
+        }
+        this.hasEaten = true; // Set this.hasEaten to true
         return true;
       } else {
         return false;
